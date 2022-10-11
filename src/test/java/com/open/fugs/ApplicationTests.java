24a29144797e411b29aede0d4028ceaa944cae55
Jefferson Alves <jeffersonalmeida16@outlook.com>
2022-10-11 07:47:38 -0300

    initial service settings

---
+++ src/test/java/com/open/fugs/ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.open.fugs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void doSomething() {
+        assertTrue(true);
+    }
+
+}
